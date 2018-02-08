f04e7bfbfab0edc5b8f8df14c916899dd935c58d
Marcus Klein <marcus.klein@live.com>
2018-02-08 10:36:06 +0100

    add react-hot-loader

--- src/core/components/Main.jsx
+++ src/core/components/Main.jsx
@@ -1,27 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
-const { Header, Content, Footer, Sider } = Layout;
-const SubMenu = Menu.SubMenu;
+import { Layout, Menu, Breadcrumb, Icon } from "antd";
 
-class SiderDemo extends React.Component {
+export default class SiderDemo extends React.Component {
   state = {
-    collapsed: false,
+    collapsed: false
   };
-  onCollapse = (collapsed) => {
+  onCollapse = collapsed => {
     console.log(collapsed);
     this.setState({ collapsed });
-  }
+  };
+  
   render() {
     return (
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider
+      <Layout style={{ minHeight: "100vh" }}>
+        <Layout.Sider
           collapsible
           collapsed={this.state.collapsed}
           onCollapse={this.onCollapse}
         >
           <div className="logo" />
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
             <Menu.Item key="1">
               <Icon type="pie-chart" />
               <span>Option 1</span>
@@ -30,45 +28,53 @@ class SiderDemo extends React.Component {
               <Icon type="desktop" />
               <span>Option 2</span>
             </Menu.Item>
-            <SubMenu
+            <Menu.SubMenu
               key="sub1"
-              title={<span><Icon type="user" /><span>User</span></span>}
+              title={
+                <span>
+                  <Icon type="user" />
+                  <span>User</span>
+                </span>
+              }
             >
               <Menu.Item key="3">Tom</Menu.Item>
               <Menu.Item key="4">Bill</Menu.Item>
               <Menu.Item key="5">Alex</Menu.Item>
-            </SubMenu>
-            <SubMenu
+            </Menu.SubMenu>
+            <Menu.SubMenu
               key="sub2"
-              title={<span><Icon type="team" /><span>Team</span></span>}
+              title={
+                <span>
+                  <Icon type="team" />
+                  <span>Team</span>
+                </span>
+              }
             >
               <Menu.Item key="6">Team 1</Menu.Item>
               <Menu.Item key="8">Team 2</Menu.Item>
-            </SubMenu>
+            </Menu.SubMenu>
             <Menu.Item key="9">
               <Icon type="file" />
               <span>File</span>
             </Menu.Item>
           </Menu>
-        </Sider>
+        </Layout.Sider>
         <Layout>
-          <Header style={{ background: '#fff', padding: 0 }} />
-          <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
+          <Layout.Header style={{ background: "#fff", padding: 0 }} />
+          <Layout.Content style={{ margin: "0 16px" }}>
+            <Breadcrumb style={{ margin: "16px 0" }}>
               <Breadcrumb.Item>User</Breadcrumb.Item>
               <Breadcrumb.Item>Bill</Breadcrumb.Item>
             </Breadcrumb>
-            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+            <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
               Bill is a cat.
             </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>
+          </Layout.Content>
+          <Layout.Footer style={{ textAlign: "center" }}>
             Ant Design ©2016 Created by Ant UED
-          </Footer>
+          </Layout.Footer>
         </Layout>
       </Layout>
     );
   }
 }
-
-ReactDOM.render(<SiderDemo />, document.getElementById('root'));
